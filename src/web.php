6edbdd92808e75794a0d50eff2a4fd360606ebe4
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2025-07-01 23:53:32 +0800

    add web version

---
+++ src/web.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Core\ShortestWord;
+
+$shortest_word = "";
+$shortest_length = null;
+
+if (!empty($_POST['sentence'])) {
+    $value = $_POST['sentence'];
+
+    $short = new ShortestWord($value);
+    $shortest_word = $short->getWord();
+    $shortest_length = $short->getLength();
+}
+?>
+
+<html>
+    <head>
+        <title>Shortest word detection</title>
+    </head>
+    <body>
+
+        <?php
+            if (!empty($_POST['sentence'])) {
+                echo '<p>OUTPUT: ' . $shortest_length . ' - BECAUSE THE SHORTEST WORD IS "' . $shortest_word . '"</p>';
+            }
+        ?>
+
+        <form method="POST">
+            <label style="margin-bottom: 8px;">Sentence</label><br>
+            <textarea rows="4" name="sentence"></textarea><br><br>
+
+            <input type="submit" value="Submit">
+        </form>
+
+    </body>
+</html>
